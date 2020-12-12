78cd7eefb28f29bfaa3661b6b53d2e38dc10cb30
Anthony M <anthony.moffa@thoughtbot.com>
2020-12-12 18:54:39 -0500

    Add RN splash screen

--- App.tsx
+++ App.tsx
@@ -1,20 +1,27 @@
 import "react-native-gesture-handler"
 import React, { FunctionComponent, useEffect, useState } from "react"
+import AsyncStorage from "@react-native-async-storage/async-storage"
+import SplashScreen from "react-native-splash-screen"
 
 import MainNavigator from "./src/MainNavigator"
 import { AuthenticationProvider } from "./src/AuthenticationContext"
-import AsyncStorage from "@react-native-async-storage/async-storage"
 
 const App: FunctionComponent = () => {
+  const [isLoading, setIsLoading] = useState<boolean>(true)
   const [authenticationToken, setAuthenticationToken] = useState<string | null>(
     null,
   )
 
   useEffect(() => {
-    AsyncStorage.getItem("authenticationToken").then(setAuthenticationToken)
+    AsyncStorage.getItem("authenticationToken")
+      .then(setAuthenticationToken)
+      .finally(() => {
+        setIsLoading(false)
+        SplashScreen.hide()
+      })
   }, [])
 
-  return (
+  return isLoading ? null : (
     <AuthenticationProvider storedToken={authenticationToken}>
       <MainNavigator />
     </AuthenticationProvider>
